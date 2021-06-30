1e693cf67ecfcc7ed1ce77831d431f4e47fdb20b
Ivan Voskoboinyk <ivan@prezly.com>
2021-06-30 13:05:04 +0300

    Set code style check paths

--- ecs.php
+++ ecs.php
@@ -1,7 +1,14 @@
 <?php
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ContainerConfigurator $config) {
+return static function (ContainerConfigurator $config): void {
+    $config->parameters()
+        ->set(Option::PATHS, [
+            __DIR__ . '/lib',
+            __DIR__ . '/tests',
+        ]);
+
     (require __DIR__ . '/vendor/prezly/code-style/ecs.php')($config);
 };
